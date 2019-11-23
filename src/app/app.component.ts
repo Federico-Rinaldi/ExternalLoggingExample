2b8d48d190aab34bcea37eb8f4a50db08935c38e
Federico Rinaldi <chicco@djsonic.it>
2019-11-23 22:44:07 +0100

    Project initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'logging';
+
+  generate(){
+    throw new Error("Error during app execution");
+  }
+}
---
+++ src/app/exception-handler.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, ErrorHandler } from '@angular/core';
+import { HttpErrorResponse, HttpHeaders, HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExceptionHandlerService implements ErrorHandler{
+
+constructor(private http:HttpClient) { }
+
+handleError(error: Error | HttpErrorResponse | TypeError): void {
+  const webHook = 'https://hooks.slack.com/services/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';
+  const moreInfo = {
+    author_name : window.location.href,
+    color:'#764FA5',
+    title: 'Error on Logging Angular APP',
+    text: error.message
+  }
+  const message = {
+    channel:'#log',
+    text: error.message,
+    attachments : moreInfo
+  }
+  const options = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' })
+  }
+
+  this.http.post(webHook,message,{...options,responseType:'text'}).subscribe(resp=>{console.warn('Sent error to slack',resp)});
+}
+
+}
